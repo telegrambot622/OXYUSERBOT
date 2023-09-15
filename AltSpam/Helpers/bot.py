b4ccc095e27985877743bd276d8ac35a24f9e8b9
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-09-15 21:31:17 +0530

    AltSpam

--- AltSpam/Helpers/bot.py
+++ AltSpam/Helpers/bot.py
@@ -1,1 +1,43 @@
+import sys
+from config import API_ID, API_HASH, BOT_TOKEN, LOG_GROUP_ID
+from pyrogram import Client
+from AltSpam.logging import LOGGER
 
+
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="AltSpam",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            in_memory=True
+        )
+
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        if get_me.last_name:
+            self.name = get_me.first_name + "" + get_me.last_name
+        else:
+            self.name = get_me.last_name
+        self.username = get_me.username
+        self.mention = get_me.mention
+        self.id = get_me.id
+        
+        await self.get_chat_member(LOG_GROUP_ID, self.id)
+        try:
+            LOGGER("AltSpam").info(f"Started As {self.name} !")
+            await self.send_message(LOG_GROUP_ID, f"**» ʙᴏᴛ sᴛᴀʀᴛᴇᴅ : {self.mention}**")
+        except:
+            LOGGER(__name__).error("Bot Has Failed To Access The Log Group !")
+            sys.exit()
+
+
+    async def stop(self):
+        await super().stop()
+        
+
+app = Bot()
