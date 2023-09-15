4de1aa7c1bd823eeab41c47b442b9ab7c3832a11
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-09-15 21:07:24 +0530

    AltSpam

--- AltSpam/__init__.py
+++ AltSpam/__init__.py
@@ -1,1 +1,22 @@
+import time
+from aiohttp import ClientSession
+from AltSpam.Helpers import app, github, one, two, three, four, five
 
+
+__Version__ = "1.0.0"
+
+boot = time.time()
+
+app = app
+
+one = one
+
+two = two
+
+three = three
+
+four = four
+
+five = five
+
+aiohttpsession = ClientSession()
---
+++ AltSpam/misc.py
@@ -0,0 +1,42 @@
+import config
+import socket
+import heroku3
+from AltSpam.logging import LOGGER
+
+
+HAPP = None
+
+
+def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(config.HEROKU_API_KEY),
+    "https",
+    str(config.HEROKU_APP_NAME),
+    "HEAD",
+    "main",
+]
+
+
+def heroku():
+    global HAPP
+    if is_heroku:
+        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
+            try:
+                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
+                HAPP = Heroku.app(config.HEROKU_APP_NAME)
+                LOGGER(__name__).info("Heroku App Configured Successfully !")
+            except BaseException:
+                LOGGER(__name__).warning("Add Heroku API Key And Your App Name In The Heroku !")
+                
+                
