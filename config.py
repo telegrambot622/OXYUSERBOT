8fe391a0a8f729e443753f9580da865ffae4772f
OXYEGN <129831825+PRADHAN474@users.noreply.github.com>
2023-09-18 02:27:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(getenv("API_ID", None))
 API_HASH = getenv("API_HASH", None)
 SESSION1 = getenv("SESSION1", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5180447182").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5059737154").split()))
 
 
 #-------------------------------- OPTIONAL -------------------------------------#
@@ -23,19 +23,19 @@ SESSION3 = getenv("SESSION3")
 SESSION4 = getenv("SESSION4")
 SESSION5 = getenv("SESSION5")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ExoticHero/AltSpam")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PRADHAN474/OXYUSERBOT")
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-EXTRA_IMG = getenv("EXTRA_IMG", "https://graph.org/file/b9fa0e997622ce29325fe.jpg")
+EXTRA_IMG = getenv("EXTRA_IMG", "https://graph.org/file/b0825ba6490d2aa6a6afd.jpg")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5180447182 5518687442").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5059737154 5911205161").split()))
 
 for y in OWNER_ID:
     SUDO_USERS.append(y)
